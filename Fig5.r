67e088832c8f265ffba68bbfae22fd5f107ed7eb
Manni-Z <82054221+Manni-Z@users.noreply.github.com>
2021-04-09 00:19:22 -0400

    Update Fig5.r

--- Fig5.r
+++ Fig5.r
@@ -6,12 +6,12 @@ Right plots: eta = 1.7
 
 # generate inputs
 x <- seq(-6, 6, 0.01)
-## left, for the right graphs just change the seed and eta
+## left plots, change seed and eta to get the right ones
 # generate weights and biases
 set.seed(45)   # right: 159
 n <- 10000
 eta <- 1.3   # right: 1.7
-# sample u a 
+# sample u a, importance sampling
 tlike <- function(x, eta) {
   (1 + x^2/(eta-1))^(-(eta-1)/2)
 }
@@ -24,6 +24,7 @@ maxDens <- max(xy$fit)
 xy$accepted <- with(xy, random <= fit/maxDens)
 xy <- xy[xy$accepted, ]
 cand <- xy$proposed*sample(c(-1,1), length(xy$proposed), replace = T)
+
 u_1 <- sample(cand, n, replace = T)
 a <- sample(cand, n, replace = T)
 b_k <- rnorm(1)
@@ -34,14 +35,11 @@ nn_tanh <- function(input, ba=a, wu=u_1, bb=b_k, wv=v_k){
   f <- b_k + sum(v_k*h)
   return(f)
 }
-# generate prior over function
+# draw prior over function
 f_k11 <- lapply(x, nn_tanh)
 # generate weights and biases
 set.seed(32456)   # right: 159
-# xx <- rgamma(n,shape = eta/2+1, rate = (eta-1)/2)
-# A <- xx^(-1/2)
-# a <- rt(n, eta-1)
-# u_1 <- rt(n, eta-1)
+# importance sampling
 xy <- data.frame(proposed = runif(N, min = 0, max = xrange))
 xy$fit <- tlike(x = xy$proposed, eta=eta)/2
 xy$random <- runif(N, min = 0, max = 1)
@@ -53,7 +51,7 @@ u_1 <- sample(cand, n, replace = T)
 a <- sample(cand, n, replace = T)
 b_k <- rnorm(1)
 v_k <- rnorm(n, sd = rep(sqrt(1/n), n))
-# generate prior over function
+# draw another prior over function
 f_k12 <- lapply(x, nn_tanh)
 df11 <- data.frame(matrix(c(as.numeric(x), as.numeric(f_k11)), byrow = F, ncol = 2))
 names(df11) <- c('x', 'Prior_over_function')
